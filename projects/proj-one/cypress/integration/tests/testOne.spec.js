699c32c87d8da79c0e7cdcd59a0c7430ccc92330
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2021-12-27 13:25:27 +0530

    test

--- projects/proj-one/cypress/integration/tests/testOne.spec.js
+++ projects/proj-one/cypress/integration/tests/testOne.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
 describe("Project One Test", () => {
-  it("Test Home Load", () => {});
+  it("Test Home Load", () => {
+    cy.visit("http://localhost:3000/");
+  });
 });
