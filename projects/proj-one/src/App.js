993528925b8849208c748d753560c5edd5ec8d46
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2021-12-28 09:01:32 +0530

    deployment workflow updated

--- projects/proj-one/src/App.js
+++ projects/proj-one/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Project One!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload
+          Edit <code>src/App.js</code> and save to reload - Proj 1
         </p>
         <a
           className="App-link"
