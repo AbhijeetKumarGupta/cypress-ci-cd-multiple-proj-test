17f817cecfef5fd84b8a8291c189745fa4eb745d
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2023-11-22 11:23:45 +0530

    Update App.js

--- projects/proj-two/src/App.js
+++ projects/proj-two/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Project Two!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload...
         </p>
         <a
           className="App-link"
